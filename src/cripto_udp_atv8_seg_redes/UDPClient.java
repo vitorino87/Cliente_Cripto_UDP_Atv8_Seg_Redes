414e23c70ee8a50038c74ead4152a39cf4e4eca4
vitorino87 <brother_87_22@hotmail.com>
2018-10-22 15:21:34 -0200

    melhorias realizadas em 22-10-18

--- src/cripto_udp_atv8_seg_redes/UDPClient.java
+++ src/cripto_udp_atv8_seg_redes/UDPClient.java
@@ -15,8 +15,14 @@ class UDPClient {
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[1024];
         String resp = "n";
-        while (!resp.equals("s") || !resp.equals("S")) {
+        while (!resp.equals("s") || !resp.equals("S")) { 
+            System.out.println("******************************************************");
+            System.out.println("Digite a mensagem que você deseja enviar:");
             String sentence = inFromUser.readLine();
+            System.out.println("Digite a senha para criptografar");
+            String senha = inFromUser.readLine();
+            StringEncrypter c = new StringEncrypter(senha);
+            sentence = c.encrypt(sentence);
             sendData = sentence.getBytes();
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
             clientSocket.send(sendPacket);
@@ -26,6 +32,10 @@ class UDPClient {
             System.out.println("Do servidor:" + modifiedSentence);
             System.out.println("Deseja sair? (s/n)");
             resp = inFromUser.readLine();
+            receivePacket = null;
+            receiveData = null;
+            receiveData = new byte[1024];
+            System.out.println("******************************************************");
         }
 
         clientSocket.close();
